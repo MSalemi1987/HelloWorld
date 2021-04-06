e1d3c16eb8e1021a1bee9c01d9fca0370065b6d6
saeedmou <saeed.mousavian@gmail.com>
2021-04-06 09:50:45 +0430

    Add project files.

---
+++ HelloWorld/frmMain.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace HelloWorld
+{
+    partial class frmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSum = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSum
+            // 
+            this.btnSum.Location = new System.Drawing.Point(12, 12);
+            this.btnSum.Name = "btnSum";
+            this.btnSum.Size = new System.Drawing.Size(75, 23);
+            this.btnSum.TabIndex = 0;
+            this.btnSum.Text = "button1";
+            this.btnSum.UseVisualStyleBackColor = true;
+            this.btnSum.Click += new System.EventHandler(this.btnSum_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSum);
+            this.Name = "frmMain";
+            this.Text = "Project Hello V 1.0";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSum;
+    }
+}
+
---
+++ HelloWorld/frmMain.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWorld
+{
+    public partial class frmMain : Form
+    {
+        private double Pi = 3.14;
+        private int k;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            
+             this.k = sum(1, 4);
+            Debug.WriteLine(k);
+            //MessageBox.Show("Ke=  " + k);
+
+        }
+
+        private int sum(int x, int y)
+        {
+            int s = x + y;
+            return s;
+
+        }
+
+        private void btnSum_Click(object sender, EventArgs e)
+        {
+            Debug.WriteLine(this.k);
+            MessageBox.Show("Ke=  " + this.k);
+        }
+    }
+}
